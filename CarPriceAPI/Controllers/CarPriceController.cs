91dfc71de320cc99849aed90a7fde2a6dc3b51c1
maxumka1792 <maxumka1792@gmail.com>
2021-03-10 14:43:41 +0700

    Init

---
+++ CarPriceAPI/Controllers/CarPriceController.cs
@@ -0,0 +1,19 @@
+﻿using CarPriceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPriceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarPriceController : ControllerBase
+    {
+        [HttpPost]
+        public JsonResult CalculatePrice(CarModel carModel)
+        {
+            if (carModel is null)
+            {
+
+            }
+        }
+    }
+}
---
+++ CarPriceAPI/JsonResults/BadJsonResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarPriceAPI.JsonResults
+{
+    internal sealed record Error(int Id, string Message);
+
+    internal sealed class BadJsonResult
+    {
+        //private const 
+    }
+}
